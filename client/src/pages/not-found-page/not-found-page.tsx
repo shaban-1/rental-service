51bca390f5897ce7b159dea13e2f11343067d6a9
shaban-1 <shaban-azizov@mail.ru>
2025-05-08 22:48:00 +0300

    feat: реализовал лабораторную работу 1 - настройка окружения и создание компонентов
    1. Настройка проекта:
    - Инициализировал проект на Vite + React с TypeScript
    - Установил основные зависимости (react-router-dom, redux toolkit)
    - Настроил ESLint и конфигурацию TypeScript
    - Добавил скрипты для запуска проекта (npm start)
    
    2. Структура проекта:
    - Создал папки components и pages для модульной структуры
    - Перенес разметку из папки markup в public
    - Реорганизовал точки входа (index.tsx)
    
    3. Реализовал компоненты:
    - Компонент карточки CitiesCard для MainPage
    - Компонент карточки NearPlacesCard для OfferPage
    - Корневой компонент App
    
    4. Создал страницы:
    - Главная страница (MainPage)
    - Страница входа (LoginPage)
    - Страница избранного (FavoritesPage)
    - Страница предложения (OfferPage)
    - Страница 404 (NotFoundPage)
    
    5. Дополнительно:
    - Настроил стили и подключил main.css
    - Исправил все HTML-атрибуты для React (className, tabIndex и др.)
    - Проверил работоспособность всех страниц

---
+++ client/src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,45 @@
+function NotFoundPage() {
+	return (
+		<div className="page page--gray">
+			<header className="header">
+				<div className="container">
+					<div className="header__wrapper">
+						<div className="header__left">
+							<a className="header__logo-link" href="/">
+							<img 
+								className="header__logo" 
+								src="/img/logo.svg" 
+								alt="Rent service logo" 
+								width="81" 
+								height="41" 
+								/>
+							</a>
+						</div>
+						<nav className="header__nav">
+							<ul className="header__nav-list">
+								<li className="header__nav-item user">
+									<a className="header__nav-link header__nav-link--profile" href="#">
+										<div className="header__avatar-wrapper user__avatar-wrapper"></div>
+										<span className="header__user-name user__name">Myemail@gmail.com</span>
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
+			<main className="page__main page__main--not-found">
+				<div className="container">
+					<section className="not-found">
+						<h1 className="not-found__title">404</h1>
+						<p className="not-found__text">Page not found</p>
+						<a className="not-found__link" href="/">
+						Return to main page
+						</a>
+					</section>
+				</div>
+			</main>
+		</div>
+	);
+}
+export { NotFoundPage };
