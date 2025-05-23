7ae7ab7d9f0ad4c30960a70c7e134835a0c92497
shaban-1 <shaban-azizov@mail.ru>
2025-05-23 09:54:37 +0300

    feat(server): реализовал дополнительный функционал отзывов, избранного и JWT-аутентификации - Реализовал функции addReview и getReviewsByOfferId - Настроил маршруты и адаптеры отзывов - Реализовал работу с избранным: getFavoriteOffers и toggleFavorite - Реализована JWT-аутентификация: login, checkAuth, logout + middleware - Все маршруты протестированы в Postman, работает корректно

--- server/routes/offerRoutes.js
+++ server/routes/offerRoutes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import upload from '../middleware/upload.js';
-import { createOffer, getAllOffers, getFullOffer  } from '../controllers/offerController.js';
+import { createOffer, getAllOffers, getFullOffer, getFavoriteOffers, toggleFavorite } from "../controllers/offerController.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
+
+
 
 const router = new Router();
 
 router.get("/offers", getAllOffers);
 router.get('/offers/:id', getFullOffer);
 router.post("/offers",upload.fields([ { name: 'previewImage', maxCount: 1 }, { name: 'photos', maxCount: 6 } ]), createOffer)
+
+router.get('/favorite',authenticateToken, getFavoriteOffers);
+router.post('/favorite/:offerId/:status', authenticateToken, toggleFavorite);
 
 export default router;
